f2a598fcd3e68f2156124fa50772622449c6ad8a
shoet <32211937+shoet@users.noreply.github.com>
2025-09-15 10:47:20 +0900

    カルーセルの作成

---
+++ shiftb-lp-site/src/app/_components/Carousel/index.tsx
@@ -0,0 +1,116 @@
+"use client";
+import clsx from "clsx";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay, Navigation } from "swiper/modules";
+import { CSSProperties, useEffect, useMemo, useState } from "react";
+
+// https://swiperjs.com/react
+
+/**
+ * 3秒おきに境目ありでスクロールするカルーセル
+ */
+export const Carousel = () => {
+  return (
+    <Swiper
+      className={clsx("w-[500px] h-[300px]")}
+      loop={true}
+      pagination={true}
+      modules={[Pagination, Autoplay]}
+      autoplay={{ delay: 3000 }}
+    >
+      <SwiperSlide className={clsx("bg-red-500")}>hoge</SwiperSlide>
+      <SwiperSlide className={clsx("bg-blue-500")}>hoge</SwiperSlide>
+      <SwiperSlide className={clsx("bg-yellow-500")}>hoge</SwiperSlide>
+      <SwiperSlide className={clsx("bg-green-500")}>hoge</SwiperSlide>
+      <SwiperSlide className={clsx("bg-purple-500")}>hoge</SwiperSlide>
+    </Swiper>
+  );
+};
+
+/**
+ * 自動で境目なしにスクロールするカルーセル
+ */
+export const CarouselContinues = () => {
+  return (
+    <Swiper
+      style={
+        {
+          "--swiper-wrapper-transition-timing-function": "linear",
+        } as CSSProperties
+      }
+      // className={clsx("w-[500px] h-[300px]")}
+      className={clsx("[&>.swiper-wrapper]:items-center")} // 子要素の.swiper-wrapperに対してスタイルを当てる
+      speed={3000}
+      autoplay={{ delay: 0 }}
+      slidesPerView={2}
+      spaceBetween={30}
+      loop={true}
+      freeMode={true}
+      modules={[Pagination, Autoplay]}
+    >
+      <SwiperSlide className={clsx("bg-red-500")}>
+        <div className={clsx("w-[200px] h-[100px]")}>hoge</div>
+      </SwiperSlide>
+      <SwiperSlide className={clsx("bg-blue-500")}>
+        <div className={clsx("w-[200px] h-[150px]")}>hoge</div>
+      </SwiperSlide>
+      <SwiperSlide className={clsx("bg-yellow-500")}>hoge</SwiperSlide>
+      <SwiperSlide className={clsx("bg-green-500")}>hoge</SwiperSlide>
+      <SwiperSlide className={clsx("bg-purple-500")}>hoge</SwiperSlide>
+    </Swiper>
+  );
+};
+
+/**
+ * 中央だけズームされるカルーセル
+ */
+export const CarouselWithCenterZoom = () => {
+  const [activeIndex, setActiveIndex] = useState<number>();
+
+  const currentIndex = useMemo(() => {
+    return activeIndex || 0;
+  }, [activeIndex]);
+
+  return (
+    <Swiper
+      style={
+        {
+          "--swiper-navigation-size": "50px", // prev/nextボタンのサイズ
+          "--swiper-navigation-sides-offset": "100px", // prev/nextボタンのX位置
+        } as CSSProperties
+      }
+      className={clsx(
+        "[&>.swiper-wrapper]:items-center", // wrapperの中央揃え
+        "[&_.swiper-slide-active]:border-2 [&_.swiper-slide-active]:border-yellow-300 [&_.swiper-slide-active]:scale-150", // swiper-slideが中心に来たときの装飾
+        "[&_.swiper-button-prev]:rounded-full [&_.swiper-button-prev]:bg-blue-300 [&_.swiper-button-prev]:p-2", // prevボタンの装飾
+        "[&_.swiper-button-next]:rounded-full [&_.swiper-button-next]:bg-blue-300 [&_.swiper-button-next]:p-2", // nextボタンの装飾
+        // "[&_.swiper-slide]:!w-[200px]", // swiper-slideの幅
+      )} // 子要素の.swiper-wrapperに対してスタイルを当てる
+      slidesPerView={3}
+      spaceBetween={30}
+      loop={true}
+      pagination={{
+        clickable: true,
+      }}
+      centeredSlides // アクティブなスライドを中心に
+      navigation={{
+        enabled: true,
+      }}
+      modules={[Navigation]}
+    >
+      <SwiperSlide className={clsx("bg-red-500")}>
+        <div className={clsx("h-[100px]")}>0</div>
+      </SwiperSlide>
+      <SwiperSlide className={clsx("bg-blue-500")}>
+        <div className={clsx("h-[150px]")}>1</div>
+      </SwiperSlide>
+      <SwiperSlide className={clsx("bg-yellow-500")}>2</SwiperSlide>
+      <SwiperSlide className={clsx("bg-green-500")}>3</SwiperSlide>
+      <SwiperSlide className={clsx("bg-purple-500")}>4</SwiperSlide>
+    </Swiper>
+  );
+};
--- shiftb-lp-site/src/app/page.tsx
+++ shiftb-lp-site/src/app/page.tsx
@@ -1,23 +1,39 @@
 import clsx from "clsx";
-import { Carousel } from "./_components/Carousel";
-
+import {
+  Carousel,
+  CarouselContinues,
+  CarouselWithCenterZoom,
+} from "./_components/Carousel";
 export default function Home() {
   return (
     <div>
       <div className={clsx("bg-[url(/background/corporate_1.jpg)]")}>
-        <div className={clsx("h-dvh w-full p-4 flex justify-center")}>
+        <div
+          className={clsx(
+            "h-dvh w-full p-4 flex justify-center max-w-[1440px] mx-auto",
+          )}
+        >
           <div className={clsx("flex-1", "grid grid-cols-2 gap-4")}>
             <div>
-              <div className={clsx("relative h-[50px]")}>
+              <div className={clsx("mx-auto max-w-[375px] pl-6")}>
                 <div
                   className={clsx(
-                    "absolute top-0 -left-[100px] translate-y-[10px] z-0",
-                    'before:content-[""] h-[30px] w-full bg-yellow-300',
+                    "relative z-0 inline whitespace-pre text-sm font-medium leading-[20.27px] md:whitespace-normal md:text-xl",
                   )}
-                ></div>
-                <div className={clsx("absolute top-0 text-xl font-bold")}>
-                  Webエンジニア特化型スクール
-                </div>
+                >
+                  <div className={clsx("text-xl font-bold")}>
+                    Webエンジニア特化型スクール
+                  </div>
+                  <div
+                    className={clsx(
+                      "absolute z-[20]  ml-[-1000px] h-5 w-[50vw] translate-y-[calc(-40%-1px)] rounded-br-sm bg-yellow-300 pl-[calc(1000px+49.5vw)]",
+                      " after:absolute after:right-[-13px] after:top-0 after:h-5 after:w-[14px] after:bg-yellow-300 after:content-[''] ",
+                      "after:[clip-path:polygon(0_0,100%_0,0_100%)] md:ml-[-500%] md:h-8 md:w-[50vw] md:pl-[calc(50 z-00%+108%)] md:after:right-[-22.7px] md:after:h-8 md:after:w-6",
+                      // "absolute top-0 z-[-2]",
+                      // 'after:content-[""] h-[30px] w-full bg-yellow-300',
+                    )}
+                  ></div>{" "}
+                </div>{" "}
               </div>
               <div className={clsx("mt-4")}>
                 <span className={clsx("text-3xl font-bold")}>
@@ -40,6 +56,8 @@ export default function Home() {
             </div>
             <div>
               <Carousel />
+              <CarouselContinues />
+              <CarouselWithCenterZoom />
             </div>
           </div>
         </div>
